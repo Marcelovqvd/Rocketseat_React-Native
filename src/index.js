b6f412f8e213063bf10f75b2693265aecb32c0d8
Marcelo <marcelovqvd@gmail.com>
2020-06-05 14:35:54 -0300

    styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 export default function App() {
-  return <View />
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7519c1" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello Gostack</Text>
+      </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#7519c1',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title: {
+      color: '#fff',
+      fontSize: 32,
+      fontWeight: 'bold',
+    }
+})
