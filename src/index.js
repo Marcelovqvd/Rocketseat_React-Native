f552aafc0539f14762895da8610b9d1cc3932671
Marcelo <marcelovqvd@gmail.com>
2020-06-05 18:14:12 -0300

    API e layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {SafeAreaView, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    api.get('/categories.php').then(response => {
+      setMeals(response.data.categories);
+      console.log('->', meals)
+    })
+  }, [])
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7519c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello Gostack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Título</Text>
+      <FlatList 
+        data={meals}
+        keyExtractor={meal => meal.idCategory}
+        renderItem={({ item: meal }) => (
+          <Text style={styles.categories}>{meal.strCategory}</Text>
+        )}
+      />
+      </SafeAreaView>
     </>
   )
 }
@@ -16,12 +34,15 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#7519c1',
-      justifyContent: 'center',
-      alignItems: 'center',
     },
     title: {
       color: '#fff',
       fontSize: 32,
+      fontWeight: 'bold',
+    },
+    categories: {
+      color: '#fff',
+      fontSize: 16,
       fontWeight: 'bold',
     }
 })
