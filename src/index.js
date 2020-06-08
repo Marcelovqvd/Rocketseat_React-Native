69b69c3c458a38c70a2a4d5ab8afa86721daf3f6
Marcelo <marcelovqvd@gmail.com>
2020-06-08 12:06:50 -0300

    prettier, eslint e editorConfig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {SafeAreaView, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
+import {View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity, button} from 'react-native';
 
 import api from './services/api';
 
@@ -9,22 +9,49 @@ export default function App() {
   useEffect(() => {
     api.get('/categories.php').then(response => {
       setMeals(response.data.categories);
-      console.log('->', meals)
     })
   }, [])
+
+  function handleProject() {
+    setMeals('');
+  }
+
+  async function mount() {
+    await api.get('./categories.php').then(response => {
+      setMeals(response.data.categories)
+    });
+  }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7519c1" />
       <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Título</Text>
+      <Text style={styles.title}>Sub-título</Text>
       <FlatList 
+        justifyContent={'center'}
+        alignItems={'center'}
+        style={styles.container}
         data={meals}
         keyExtractor={meal => meal.idCategory}
         renderItem={({ item: meal }) => (
           <Text style={styles.categories}>{meal.strCategory}</Text>
         )}
       />
+      <TouchableOpacity 
+        activeOpacity={0.6} 
+        style={styles.button} 
+        onPress={handleProject}
+      >
+        <Text style={styles.buttonText}>Apagar tudo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        activeOpacity={0.6} 
+        style={styles.button} 
+        onPress={mount}
+      >
+        <Text style={styles.buttonText}>Montar tudo</Text>
+      </TouchableOpacity>
       </SafeAreaView>
     </>
   )
@@ -39,10 +66,25 @@ const styles = StyleSheet.create({
       color: '#fff',
       fontSize: 32,
       fontWeight: 'bold',
+      textAlign: 'center',
+      marginTop: 20,
     },
     categories: {
       color: '#fff',
       fontSize: 16,
       fontWeight: 'bold',
+    },
+    button: {
+      alignSelf: "stretch",
+      backgroundColor: '#fff',
+      margin: 20,
+      height: 50,
+      borderRadius: 4,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonText: {
+      fontWeight: 'bold',
+      fontSize: 16,
     }
 })
